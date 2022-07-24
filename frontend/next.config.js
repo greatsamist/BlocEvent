38505c9bd89bf04b5d83270403df3d5b7634d47b
Samuel Ojo <ifegbenga@gmail.com>
2022-07-24 03:59:08 +0100

    used getServerProps

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,31 +3,12 @@
  * @type {import('next').NextConfig}
  */
 
-// const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
-  // compress: true,
-  // swcMinify: false,
-  // reactStrictMode: true,
-  // poweredByHeader: false,
-  // productionBrowserSourceMaps: false,
-  // compiler: {
-  //   styledComponents: true,
-  // },
-  // i18n: {
-  //   locales: ["en-US"],
-  //   defaultLocale: "en-US",
-  //   localeDetection: true,
-  // },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
   images: {
     loader: "akamai",
     path: "",
   },
   trailingSlash: true,
-  // assetPrefix: isProd ? "https://blocevent-r5euy.spheron.app/" : "",
 };
 
 module.exports = nextConfig;
--- frontend/pages/events.tsx
+++ frontend/pages/events.tsx
@@ -1,49 +1,48 @@
 import { Fragment } from "react";
 import { NextPage } from "next";
 import { default as NextHead } from "next/head";
-// import { EventList } from "@components";
+import { EventList } from "@components";
 
-const Events: NextPage = () => {
+const Events: NextPage<eventDataProps> = ({ events }) => {
   return (
     <Fragment>
       <NextHead>
         <title>All Events | BlocEvent - A decentralized events app</title>
       </NextHead>
-      {/* <EventList events={events} /> */}
+      <EventList events={events} />
     </Fragment>
   );
 };
 
-// export async function getStaticProps() {
-//   // fetch data from api
-//   const res = await fetch("https://blockevents.herokuapp.com/events");
+export async function getServerSideProps() {
+  // fetch data from api
+  const res = await fetch("https://blockevents.herokuapp.com/events");
 
-//   const eventData = await res.json();
+  const eventData = await res.json();
 
-//   return {
-//     props: {
-//       events: eventData.data,
-//     },
-//     revalidate: 10,
-//   };
-// }
+  return {
+    props: {
+      events: eventData.data,
+    },
+  };
+}
 export default Events;
 
-// export interface eventArr {
-//   id: string;
-//   eventName: string;
-//   participantsNumber: number;
-//   eventType: string;
-//   organizers: string;
-//   category: string;
-//   eventDate: string;
-//   startTime: string;
-//   endTime: string;
-//   description: string;
-//   ticketPrice: number;
-//   eventFile: string;
-// }
+export interface eventArr {
+  id: string;
+  eventName: string;
+  participantsNumber: number;
+  eventType: string;
+  organizers: string;
+  category: string;
+  eventDate: string;
+  startTime: string;
+  endTime: string;
+  description: string;
+  ticketPrice: number;
+  eventFile: string;
+}
 
-// export interface eventDataProps {
-//   events: eventArr[];
-// }
+export interface eventDataProps {
+  events: eventArr[];
+}
