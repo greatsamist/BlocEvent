0169ba27f158161a8b05cf30a2cded587809da2e
Eric <ericvondee5@gmail.com>
2022-07-24 18:08:46 +0100

    event refactor and nftport integration

--- backend/src/event.js
+++ backend/src/event.js
@@ -8,6 +8,7 @@ import { ethers } from 'ethers'
 import { connect, resultsToObjects, SUPPORTED_CHAINS } from '@tableland/sdk'
 import { Web3Storage, getFilesFromPath } from 'web3.storage'
 import { EventValidator } from './validator/index.js'
+import axios from 'axios';
 dotenv.config()
 
 const polygonTestnet = SUPPORTED_CHAINS['polygon-mumbai']
@@ -33,7 +34,6 @@ router.post("/", upload.single('image'), async (req, res, next) => {
     try {
 
         const uploadName = ["ImageGallery", req.file.originalname].join('|')
-        console.log(uploadName)
 
         const web3Storage = new Web3Storage({ token: process.env.WEB3STORAGE_TOKEN })
         console.log(`> 🤖 calculating content ID for ${req.file.originalname}`)
@@ -50,19 +50,34 @@ router.post("/", upload.single('image'), async (req, res, next) => {
 
         const imageURI = `ipfs://${cid}/${req.file.originalname}`
       
-        await fs.unlinkSync(path.join(__dirname, `${req.file.originalname}`))
-
-        // const writeRes = await tableLand.write(`INSERT INTO blockevents_80001_450 (id, eventName, eventType, category, eventDate, startTime, endTime, description, shortDescription, organizers, participantsNumber, ticketPrice, eventFile) 
-        // VALUES (1, '${value.eventName}' , '${value.eventType}', '${value.category}', '${value.eventDate}', '${value.startTime}', '${value.endTime}', '${value.description}', '${value.shortDescription}', '${value.organizers}', ${Number(value.participantsNumber)}, ${Number(value.ticketPrice)}, '${cid}')`);
-
-        const writeRes = await tableLand.write(`INSERT INTO blockevents_80001_504 (id, eventName, eventType, category, eventDate, startTime, endTime, description, organizers, participantsNumber, ticketPrice, eventFile) 
-        VALUES ('${value.eventName.toLowerCase()}', '${value.eventName}' , '${value.eventType}', '${value.category}', '${value.eventDate}', '${value.startTime}', '${value.endTime}', '${value.description}', '${value.organizers}', ${Number(value.participantsNumber)}, ${Number(value.ticketPrice)}, '${imageURI}')`);
+        const createDocument = await tableLand.write(`INSERT INTO blockevents_80001_502 (id, eventName, eventType, category, eventDate, startTime, endTime, description, organizers, participantsNumber, ticketPrice, eventId, eventFile) 
+        VALUES ('${value.eventName.toLowerCase()}', '${value.eventName}' , '${value.eventType}', '${value.category}', '${value.eventDate}', '${value.startTime}', '${value.endTime}', '${value.description}', '${value.organizers}', ${Number(value.participantsNumber)}, ${Number(value.ticketPrice)}, '${value.eventId}', '${imageURI}')`);
         
+        /**upload and mint nft to address */
+        const filestrean = fs.createReadStream(path.join(__dirname, `${req.file.originalname}`))
+        
+        let upload = await axios({
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                "Authorization": process.env.NFTPORT_KEY,
+              },
+              method: 'post',
+              url: process.env.NFTPORT_MINTING_URL,
+              params: {
+                  chain: 'polygon', 
+                  name: value.eventName, 
+                  description: value.description, 
+                  mint_to_address: value.address
+            },
+            data: {'file': filestrean}
+        })
+        console.log(upload)
+        await fs.unlinkSync(path.join(__dirname, `${req.file.originalname}`))
         res.status(200).json({
             cid,
             imageURI,
-            writeRes,
-            //writeRess
+            createDocument,
+            transactionUrl: upload.data.transaction_external_url
         })
 
     }
@@ -82,12 +97,8 @@ router.post('/table', upload.none(), async (req, res, next) => {
         const signer = wallet.connect(provider)
         const tableLand = await await connect({ signer, network: "testnet", host: polygonTestnet.host, contract: polygonTestnet.contract, chainId: polygonTestnet.chainId })
 
-        // const { name } = await tableLand.create(
-        //     `eventName text, eventType text, category text, eventDate text, startTime text, endTime text, description text, shortDescription text, organizers text, participantsNumber int, ticketPrice int, eventFile text, primary key(id)`,
-        //     `blockevents`
-        // )
         const { name } = await tableLand.create(
-            `eventName text, eventType text, category text, eventDate text, startTime text, endTime text, description text, organizers text, participantsNumber int, ticketPrice int, eventFile text, id text, primary key(id)`,
+            `eventName text, eventType text, category text, eventDate text, startTime text, endTime text, description text, organizers text, participantsNumber int, ticketPrice int, eventId text, eventFile text, id text, primary key(id)`,
             `blockevents`
         )
         return res.status(200).json({ statusode: 200, data: name })
@@ -110,7 +121,6 @@ router.get('/', async (req, res, next) => {
 
         const events = await tableLand.read(`SELECT * FROM  blockevents_80001_504`)
 
-        console.log(events)
         const entries = resultsToObjects(events)
 
         for (const {
@@ -175,6 +185,40 @@ router.get('/query', async (req, res, next) => {
     }
 })
 
+router.post('/mint', upload.single('image'),  async(req,res,next) => {
+    try{
+        const filestrean = fs.createReadStream(path.join(__dirname, `${req.file.originalname}`))
+        
+        let upload = await axios({
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                "Authorization": "6e65e278-66d4-4d6a-bc80-ad23d6a429f4",
+              },
+              method: 'post',
+              url: "https://api.nftport.xyz/v0/mints/easy/files",
+              params: {
+                  chain: 'polygon', 
+                  name: 'Blockevent', 
+                  description: 'BlockEvent', 
+                  mint_to_address: '0xF24c148d8390a32D25897EC70f2AD5dfdF186E17'
+            },
+            data: {'file': filestrean}
+          })
+          res.status(200).json({
+            statusCode: 200,
+            message: "",
+            data: upload
+        })
 
-//module.exports = router
+    }
+    catch(e){
+        console.log(e)
+        res.status(500).json({
+            statusCode: 500,
+            error: e.message
+        })
+    }
+})
+
+
 export default router
