3d1f6f04f57d08894b70560e97ed2355f7933a31
Samuel Ojo <ifegbenga@gmail.com>
2022-07-24 04:25:33 +0100

    fixes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,15 @@
  * @type {import('next').NextConfig}
  */
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   images: {
     loader: "akamai",
     path: "",
   },
   trailingSlash: true,
+  assetPrefix: isProd ? "https://blocevent-r5euy.spheron.app/" : "",
 };
 
 module.exports = nextConfig;
